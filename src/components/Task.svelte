9eb4f2e4c116fc49d3a4e52448319c86fbf00438
Joost Faber <j.faber@hva.nl>
2021-03-05 20:56:28 +0100

    fake push

--- src/components/Task.svelte
+++ src/components/Task.svelte
@@ -21,7 +21,6 @@
         </div>
         
         <footer>
-
             <p>
                 <svg class="task-level {supportLevels[task["support-level"] -1]}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                     viewBox="0 0 44 44">
